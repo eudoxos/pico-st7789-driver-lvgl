831414c4dcaa55034f9c3ae44d18102283542074
Václav Šmilauer <eu@doxos.eu>
2021-09-20 08:00:08 +0200

    generalize to st7789 and 7735 (not yet working)

--- st7789_lowlevel.py
+++ st7789_lowlevel.py
@@ -6,61 +6,61 @@ import uctypes
 from micropython import const
 import rp2_dma
 
-ST7789_NOP = const(0x00)
-ST7789_SWRESET = const(0x01)
-ST7789_RDDID = const(0x04)
-ST7789_RDDST = const(0x09)
+ST77XX_NOP = const(0x00)
+ST77XX_SWRESET = const(0x01)
+ST77XX_RDDID = const(0x04)
+ST77XX_RDDST = const(0x09)
 
-ST7789_SLPIN = const(0x10)
-ST7789_SLPOUT = const(0x11)
-ST7789_PTLON = const(0x12)
-ST7789_NORON = const(0x13)
+ST77XX_SLPIN = const(0x10)
+ST77XX_SLPOUT = const(0x11)
+ST77XX_PTLON = const(0x12)
+ST77XX_NORON = const(0x13)
 
-ST7789_INVOFF = const(0x20)
-ST7789_INVON = const(0x21)
-ST7789_DISPOFF = const(0x28)
-ST7789_DISPON = const(0x29)
+ST77XX_INVOFF = const(0x20)
+ST77XX_INVON = const(0x21)
+ST77XX_DISPOFF = const(0x28)
+ST77XX_DISPON = const(0x29)
 
-ST7789_CASET = const(0x2A)
-ST7789_RASET = const(0x2B)
-ST7789_RAMWR = const(0x2C)
-ST7789_RAMRD = const(0x2E)
+ST77XX_CASET = const(0x2A)
+ST77XX_RASET = const(0x2B)
+ST77XX_RAMWR = const(0x2C)
+ST77XX_RAMRD = const(0x2E)
 
-ST7789_PTLAR = const(0x30)
-ST7789_MADCTL = const(0x36)
-ST7789_COLMOD = const(0x3A)
+ST77XX_PTLAR = const(0x30)
+ST77XX_MADCTL = const(0x36)
+ST77XX_COLMOD = const(0x3A)
 
 ST7789_WRCACE = const(0x55)
 
-ST7789_FRMCTR1 = const(0xB1)
-ST7789_FRMCTR2 = ST7789_PORCTRL = const(0xB2)
-ST7789_FRMCTR3 = const(0xB3)
-ST7789_INVCTR = const(0xB4)
+ST77XX_FRMCTR1 = const(0xB1)
+ST77XX_FRMCTR2 = ST7789_PORCTRL = const(0xB2)
+ST77XX_FRMCTR3 = const(0xB3)
+ST77XX_INVCTR = const(0xB4)
 ST7789_DISSET5 = const(0xB6)
 
 ST7789_GCTRL = const(0xB7)
 ST7789_GTADJ = const(0xB8)
 ST7789_VCOMS = const(0xBB)
 
-ST7789_LCMCTRL = const(0xC0)
-ST7789_IDSET = const(0xC1)
-ST7789_VDVVRHEN = const(0xC2)
-ST7789_VRHS = const(0xC3)
-ST7789_VDVS = const(0xC4)
-ST7789_VMCTR1 = const(0xC5)
+ST7735_PWCTR1 = ST7789_LCMCTRL = const(0xC0)
+ST7735_PWCTR2 = ST7789_IDSET = const(0xC1)
+ST7735_PWCTR3 = ST7789_VDVVRHEN = const(0xC2)
+ST7735_PWCTR4 = ST7789_VRHS = const(0xC3)
+ST7735_PWCTR5 = ST7789_VDVS = const(0xC4)
+ST7735_VMCTR1 = ST7789_VMCTR1 = const(0xC5)
 ST7789_FRCTRL2 = const(0xC6)
 ST7789_CABCCTRL = const(0xC7)
 
 ST7789_PWCTRL1 = const(0xD0)
-ST7789_RDID1 = const(0xDA)
-ST7789_RDID2 = const(0xDB)
-ST7789_RDID3 = const(0xDC)
-ST7789_RDID4 = const(0xDD)
+ST77XX_RDID1 = const(0xDA)
+ST77XX_RDID2 = const(0xDB)
+ST77XX_RDID3 = const(0xDC)
+ST77XX_RDID4 = const(0xDD)
 
 ST7789_GMCTRP1 = ST7789_PVGAMCTRL = const(0xE0)
 ST7789_GMCTRN1 = ST7789_NVGAMCTRL = const(0xE1)
 
-ST7789_PWCTR6 = const(0xFC)
+ST7735_PWCTR6 = ST7789_PWCTR6 = const(0xFC)
 
 
 ST77XX_MADCTL_MY = const(0x80)  # page address order (0: top to bottom; 1: bottom to top)
@@ -89,22 +89,69 @@ ST77XX_COLOR_MODE_16M = const(0x07)
 # https://github.com/adafruit/Adafruit-ST7735-Library/blob/master/Adafruit_ST7789.cpp
 # independent (?) micropython implementation:
 # https://techatronic.com/st7789-display-pi-pico/
+# st77xx c driver (for uPy), with simplified init sequence:
+# https://github.com/szampardi/st77xx_mpy
 
-class St7789:
-    def __init__(self, *, cs, dc, bl, spi, width=320, height=240, rot=1, dma=None):
+class St77xx(object):
+    def __init__(self, *, cs, dc, bl, spi, rst=None, res=(320,240), rot=1, dma=None):
+        '''
+        * *cs*: chip select pin (= slave select, SS)
+        * *dc*: data/command pin
+        * *bl*: backlight PWM pin
+        * *rst*: optional reset pin
+        * *res*: resolution tuple; greater value always comes first
+        * *rot*: display orientation (0: landscape, 1: portrait, 2: reverse landscape, 3: reverse portrait)
+        '''
         self.buf1 = bytearray(1)
         self.buf2 = bytearray(2)
         self.buf4 = bytearray(4)
 
-        self.cs,self.dc,self.bl = [(machine.Pin(p,machine.Pin.OUT) if isinstance(p,int) else p) for p in (cs,dc,bl)]
-        self.width = (width if rot%2 else height)
-        self.height = (height if rot%2 else width)
+        self.cs,self.dc,self.bl,self.rst = [(machine.Pin(p,machine.Pin.OUT) if isinstance(p,int) else p) for p in (cs,dc,bl,rst)]
+        self.bl.value(1)
+        self.bl=machine.PWM(self.bl)
+        self.width = (res[0] if rot%2 else res[1])
+        self.height = (res[1] if rot%2 else res[0])
         self.rot = rot
 
         self.dma = dma
         self.spi = spi
         self.config()
 
+    def off(self):
+        self.bl.value(0)
+    def hard_reset(self):
+        if self.rst:
+            for v in (1,0,1):
+                self.rst.value(v)
+                time.sleep(.5)
+            time.sleep(.5)
+        self.config()
+    def set_backlight(self,percent):
+        self.bl.duty_u16(percent*655)
+    def set_window(self, x, y, w, h):
+        struct.pack_into('>hh', self.buf4, 0, x, x+w-1)
+        self.write_register(ST77XX_CASET, self.buf4)
+        struct.pack_into('>hh', self.buf4, 0, y, y+h-1)
+        self.write_register(ST77XX_RASET, self.buf4)
+
+    def blit(self, x, y, w, h, buf, is_blocking=True):
+        self.set_window(x, y, w, h)
+        if self.dma: self.write_register_dma(ST77XX_RAMWR, buf, is_blocking)
+        else: self.write_register(ST77XX_RAMWR, buf)
+
+    def clear(self, color):
+        bs=128 # write pixels in chunks; makes the fill much faster
+        struct.pack_into('>h',self.buf2,0,color)
+        buf=bs*bytes(self.buf2)
+        npx=self.width*self.height
+        self.set_window(0, 0, self.width, self.height)
+        self.write_register(ST77XX_RAMWR, None)
+        self.cs.value(0)
+        self.dc.value(1)    
+        for _ in range(npx//bs): self.spi.write(buf)
+        for _ in range(npx%bs): self.spi.write(self.buf2)
+        self.cs.value(1)
+        
     def write_register(self, reg, buf=None):
         struct.pack_into('B', self.buf1, 0, reg)
         self.cs.value(0)
@@ -115,7 +162,7 @@ class St7789:
             self.spi.write(buf)
         self.cs.value(1)
 
-    def write_register_dma(self, reg, buf, is_blocking=True ):
+    def write_register_dma(self, reg, buf, is_blocking=True):
         'If *is_blocking* is False, used should call wait_dma explicitly.'
         SPI1_BASE = 0x40040000 # FIXME: will be different for another SPI bus?
         SSPDR     = 0x008
@@ -145,68 +192,124 @@ class St7789:
         time.sleep_us(1)
         self.cs.value(1)
 
+    def _run_seq(self,seq):
+        '''
+        Run sequence of (initialization) commands; those are given as list of tuples, which are either
+        `(command,data)` or `(command,data,delay_ms)`
+        '''
+        for i,cmd in enumerate(seq):
+            if len(cmd)==2: (reg,data),delay=cmd,0
+            elif len(cmd)==3: reg,data,delay=cmd
+            else: raise ValueError('Command #%d has %d items (must be 2 or 3)'%(i,len(cmd)))
+            self.write_register(reg,data)
+            if delay>0: time.sleep_ms(delay)
+
+        
+class St7735(St77xx):
+    '''There are many variants of ST7735-based LCDs, none of them seem to be working yet'''
+    def __init__(self,res,**kw):
+        suppRes=[(160,128),]
+        if res not in suppRes: raise ValueError('Unsupported resolution %s; the driver currently supports %s.'%(str(res,', '.join(str(r) for r in suppRes))))
+        super().__init__(res=res,**kw)
+    def config(self):
+        # mostly from here
+        # https://github.com/stechiez/raspberrypi-pico/blob/main/pico_st7735/st7735/ST7735.py
+        # the "blue version" only
+        init7735r=[
+            # see here for explanations: https://github.com/adafruit/Adafruit-ST7735-Library/blob/master/Adafruit_ST7735.cpp
+            (ST77XX_SWRESET, None, 150),
+            (ST77XX_SLPOUT, None, 255),
+            (ST77XX_FRMCTR1,b'\x01\x2c\x2d'),
+            (ST77XX_FRMCTR2,b'\x01\x2c\x2d'),
+            (ST77XX_FRMCTR3,b'\x01\x2c\x2d\x01\x2c\x2d'),
+            (ST77XX_INVCTR,b'\x07'),
+            (ST7735_PWCTR1,b'\xa2\x02\xb4'),
+            (ST7735_PWCTR2,b'\xc5'),
+            (ST7735_PWCTR3,b'\x0a\x00'),
+            (ST7735_PWCTR4,b'\x8a\x2a'),
+            (ST7735_PWCTR5,b'\x8a\xee'),
+            (ST7735_VMCTR1,b'\x0e'),
+            (ST77XX_INVOFF,None),
+            (ST77XX_MADCTL,bytes([ST77XX_MADCTL_ROTS[self.rot%4]])),    
+            (ST77XX_COLMOD,bytes([ST77XX_COLOR_MODE_65K | ST77XX_COLOR_MODE_16BIT])),
+            (ST77XX_CASET,bytes([0x00,0x00,0x00,0x7f])),
+            (ST77XX_RASET,bytes([0x00,0x00,0x00,0x9f])),
+            # blacktab only
+            (ST77XX_MADCTL,b'\xc0'),
+            # TODO: gamma adjustment
+            (ST77XX_NORON, None, 10),
+            (ST77XX_DISPON, None,100)
+        ]
+        init7735=[
+            # swreset
+            (ST77XX_SWRESET, None, 50),
+            # out of sleep mode
+            (ST77XX_SLPOUT, None, 100),
+            # RGB565
+            (ST77XX_COLMOD,bytes([ST77XX_COLOR_MODE_65K | ST77XX_COLOR_MODE_16BIT])),
+            # fast refresh (??)
+            (ST77XX_FRMCTR1,bytes([0x00,0x06,0x03])),
+            (ST77XX_MADCTL,bytes([0x03])),
+            (ST77XX_INVCTR,b'\x00'),
+            (ST7735_PWCTR1,b'\x02\x70'),
+            (ST7735_PWCTR2,b'\x05'),
+            (ST7735_PWCTR3,b'\x01\x02'),
+            (ST7735_VMCTR1,b'\x3c\x38'),
+            (ST7735_PWCTR6,b'\b11\b15'),
+            # (ST77XX_GMCTRP1,b'\
+            ## memory access direction
+            #(ST77XX_MADCTL, bytes([ST77XX_MADCTL_ROTS[self.rot%4]]), 0),    
+            # inverted on (?)
+            #(ST77XX_INVON, None, 10),
+            # normal display on
+            (ST77XX_NORON, None, 10),
+            # display on
+            (ST77XX_DISPON, None,100)
+        ]
+        self._run_seq(init7735r)
+
+
+class St7789(St77xx):
+    def __init__(self,res,**kw):
+        suppRes=[(320,240),]
+        if res not in suppRes: raise ValueError('Unsupported resolution %s; the driver currently supports %s.'%(str(res,', '.join(str(r) for r in suppRes))))
+        super().__init__(res=res,**kw)
     def config(self):
         init7789=[
             # out of sleep mode
-            (ST7789_SLPOUT, None, 100),     
+            (ST77XX_SLPOUT, None, 100),     
             # memory access direction
-            (ST7789_MADCTL, bytes([ST77XX_MADCTL_ROTS[self.rot%4]]), 0),    
+            (ST77XX_MADCTL, bytes([ST77XX_MADCTL_ROTS[self.rot%4]])),    
             # RGB565
-            (ST7789_COLMOD, bytes([ST77XX_COLOR_MODE_65K | ST77XX_COLOR_MODE_16BIT]), 0 ),
+            (ST77XX_COLMOD, bytes([ST77XX_COLOR_MODE_65K | ST77XX_COLOR_MODE_16BIT])),
             # front/back porch setting in normal/idle/partial modes; 3rd byte (PSEN) 0x00 = disabled
-            (ST7789_PORCTRL, b"\x0C\x0C\x00\x33\x33", 0), 
+            (ST7789_PORCTRL, b"\x0C\x0C\x00\x33\x33"), 
             # VGH=14.06V, VGL=-8.87V [Adafruit: 0x14]
-            (ST7789_GCTRL, b"\x35", 0),
+            (ST7789_GCTRL, b"\x35"),
             # [Adafruit: missing]
-            (ST7789_DISPOFF, b"\x28", 0),
+            (ST77XX_DISPOFF, b"\x28"),
             # power control [Adafruit: 0x2c]
-            (ST7789_LCMCTRL, b"\x3C", 0),
+            (ST7789_LCMCTRL, b"\x3C"),
             # power control (set VDV and VRD by register write), write VRH and VDV 
-            (ST7789_VDVVRHEN, b"\x01", 0),(ST7789_VRHS, b"\x0B", 0),(ST7789_VDVS, b"\x20", 0),      
+            (ST7789_VDVVRHEN, b"\x01"),(ST7789_VRHS, b"\x0B"),(ST7789_VDVS, b"\x20"),      
             # frame rate 60Hz
-            (ST7789_FRCTRL2, b"\x0F", 0),
+            (ST7789_FRCTRL2, b"\x0F"),
             # power control: AVDD=6.6V, AVCL=-4.8V, VDS=2.4V
-            (ST7789_PWCTRL1, b"\xA4\xA1", 0),
+            (ST7789_PWCTRL1, b"\xA4\xA1"),
             # positive voltage gamma control
-            (ST7789_PVGAMCTRL, b"\xD0\x01\x08\x0F\x11\x2A\x36\x55\x44\x3A\x0B\x06\x11\x20", 0),
+            (ST7789_PVGAMCTRL, b"\xD0\x01\x08\x0F\x11\x2A\x36\x55\x44\x3A\x0B\x06\x11\x20"),
             # negative voltage gamma control
-            (ST7789_NVGAMCTRL, b"\xD0\x02\x07\x0A\x0B\x18\x34\x43\x4A\x2B\x1B\x1C\x22\x1F", 0), 
+            (ST7789_NVGAMCTRL, b"\xD0\x02\x07\x0A\x0B\x18\x34\x43\x4A\x2B\x1B\x1C\x22\x1F"),
             # content adaptive brightness control and color enhancement: color enhancement on, high enhancement
-            (ST7789_WRCACE, bytes([0b1011_0000]), 0),
+            (ST7789_WRCACE, bytes([0b1011_0000])),
             # display on
-            (ST7789_DISPON, None,100),        
+            (ST77XX_DISPON, None,100),        
         ]
-        for reg,data,delay in init7789:
-            self.write_register(reg,data)
-            if delay>0: time.sleep_ms(delay)
-        self.bl.value(1)
+        self._run_seq(init7789)
 
-    def set_window(self, x, y, w, h):
-        struct.pack_into('>hh', self.buf4, 0, x, x+w-1)
-        self.write_register(ST7789_CASET, self.buf4)
-        struct.pack_into('>hh', self.buf4, 0, y, y+h-1)
-        self.write_register(ST7789_RASET, self.buf4)
-
-    def blit(self, x, y, w, h, buf, is_blocking=True):
-        self.set_window(x, y, w, h)
-        if self.dma: self.write_register_dma(ST7789_RAMWR, buf, is_blocking)
-        else: self.write_register(ST7789_RAMWR, buf)
-
-    def clear(self, color):
-        bs=128 # write pixels in chunks; makes the fill much faster
-        struct.pack_into('>h',self.buf2,0,color)
-        buf=bs*bytes(self.buf2)
-        npx=self.width*self.height
-        self.set_window(0, 0, self.width, self.height)
-        self.write_register(ST7789_RAMWR, None)
-        self.cs.value(0)
-        self.dc.value(1)    
-        for _ in range(npx//bs): self.spi.write(buf)
-        for _ in range(npx%bs): self.spi.write(self.buf2)
-        self.cs.value(1)
 
 if __name__=='__main__':
-    
+
     LCD_RST_PIN=15 # [unused]
     LCD_DC_PIN=8 # jgpeiro: 14
     LCD_CS_PIN=9
@@ -214,39 +317,29 @@ if __name__=='__main__':
     LCD_BKL_PIN=13 #jgpeiro: 15
     LCD_MOSI_PIN=11
     LCD_MISO_PIN=12
-        
+
     def build_square_buf(w, h, inner=[0x00,0x00]):
         top = b"\xFF\xFF"*w
         body=(b"\xFF\xFF" + bytes(inner)*(w-2) + b"\xFF\xFF")*(h-2)
         bot = b"\xFF\xFF"*w
         return top + body + bot
-        
-    def test_lcd():
-        spi = machine.SPI(
-            1, 
-            baudrate=24_000_000, 
-            polarity=0,
-            phase=0,
-            sck=machine.Pin(LCD_CLK_PIN,machine.Pin.OUT),
-            mosi=machine.Pin(LCD_MOSI_PIN,machine.Pin.OUT),
-            miso=machine.Pin(LCD_MISO_PIN,machine.Pin.IN)
-        )
-        dma=rp2_dma.DMA(0)
-        #dma=None
-        lcd = St7789(rot=1,spi=spi,dma=dma,cs=LCD_CS_PIN,dc=LCD_DC_PIN,bl=LCD_BKL_PIN)
-        lcd.clear(0x0000)
-        
+
+    def test_lcd(lcd):
+        # lcd.hard_reset()
+        lcd.set_backlight(30)
+
+        lcd.clear(0x0000)    
         # 1/4 screen pixels square with white border red backgorund 
-        w, h = 320//4, 240//4
+        w, h = lcd.width//4, lcd.height//4
         bmp = build_square_buf(w, h, [0x03,0x03])
-        
+
         t0 = time.ticks_us()
         lcd.blit(60, 60, w, h, bmp)
         t1 = time.ticks_us()
 
         print("Maximum FPS @24MHz:", 24e6/(320*240*16)) # FPS = F/(W*H*BPP)
         print("Achieved FPS:", 1/(16*(t1-t0)*1e-6))       # Note: Test only draws 1/16 of the sreen area
-        
+
         print( "Draw TSC calibration pattern")
         w, h = 15, 15
         bmp = build_square_buf(w, h, [0x00,0x00])
@@ -254,4 +347,22 @@ if __name__=='__main__':
         lcd.blit(250, 50, w, h, bmp)
         lcd.blit(250, 200, w, h, bmp)
         lcd.blit(50, 200, w, h, bmp)
-    test_lcd()
+        for p in (20,100,80,50,10,0):
+            lcd.set_backlight(p)
+            time.sleep(.5)
+
+    spi = machine.SPI(
+        1, 
+        baudrate=20_000_000, 
+        polarity=0,
+        phase=0,
+        sck=machine.Pin(LCD_CLK_PIN,machine.Pin.OUT),
+        mosi=machine.Pin(LCD_MOSI_PIN,machine.Pin.OUT),
+        miso=machine.Pin(LCD_MISO_PIN,machine.Pin.IN)
+    )
+    dma=rp2_dma.DMA(0)
+
+    lcd7789=St7789(rot=1,res=(320,240),spi=spi,dma=dma,cs=LCD_CS_PIN,dc=LCD_DC_PIN,bl=LCD_BKL_PIN,rst=LCD_RST_PIN)
+    test_lcd(lcd=lcd7789)
+    #lcd7735=St7735(rot=1,res=(160,128),spi=spi,dma=None,rst=16,dc=17,cs=18,bl=19)
+    #test_lcd(lcd=lcd7735)
